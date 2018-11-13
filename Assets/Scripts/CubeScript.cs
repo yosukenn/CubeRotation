7ebcacb532678dd12553475e28d9bf02224fecd7
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-11-13 19:28:25 +0900

    success to rotate cube

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -11,6 +11,6 @@ public class CubeScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.rotation = Quaternion.AngleAxis(45, new Vector3(0, 1, 0));
 	}
 }
