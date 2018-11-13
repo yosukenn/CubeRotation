cfeccdc1903b9e7e83e76a2c67d44c520be1ae5a
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-11-13 22:51:41 +0900

    success to rotate by my_input_number

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -19,8 +19,12 @@ public class CubeScript : MonoBehaviour {
         Text y = GameObject.Find("AxisY").GetComponent<Text>();
         Text z = GameObject.Find("AxisZ").GetComponent<Text>();
         Text angle = GameObject.Find("Angle").GetComponent<Text>();
+        float xNum = float.Parse(x.text);
+        float yNum = float.Parse(y.text);
+        float zNum = float.Parse(z.text);
+        float angleNum = float.Parse(angle.text);
 
-        transform.rotation = Quaternion.AngleAxis(45, new Vector3(0, 1, 0));
+        transform.rotation = Quaternion.AngleAxis(angleNum, new Vector3(xNum, yNum, zNum));
 
     }
 }
