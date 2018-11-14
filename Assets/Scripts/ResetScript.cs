4727905fb92df437c3303188de37fb18328c2d82
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-11-14 18:19:36 +0900

    success to reset axis_x value

---
+++ Assets/Scripts/ResetScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetScript : MonoBehaviour {
+
+    public InputField x;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ResetText() {
+        Debug.Log(x);
+        Debug.Log(x.text);
+        x.text = "0";
+        Debug.Log(x.text);
+
+    }
+}
