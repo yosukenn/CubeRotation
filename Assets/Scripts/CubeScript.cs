30f286fd19d32f1ac5f342cbe16f2ccec1ef9ca9
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-11-14 18:53:00 +0900

    temporary if branch

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -19,12 +19,17 @@ public class CubeScript : MonoBehaviour {
         Text y = GameObject.Find("AxisY").GetComponent<Text>();
         Text z = GameObject.Find("AxisZ").GetComponent<Text>();
         Text angle = GameObject.Find("Angle").GetComponent<Text>();
-        float xNum = float.Parse(x.text);
-        float yNum = float.Parse(y.text);
-        float zNum = float.Parse(z.text);
-        float angleNum = float.Parse(angle.text);
 
-        transform.rotation *= Quaternion.AngleAxis(angleNum, new Vector3(xNum, yNum, zNum));
+        if (!string.IsNullOrEmpty(x.text) && !string.IsNullOrEmpty(y.text) && !string.IsNullOrEmpty(z.text) && !string.IsNullOrEmpty(angle.text)) {
+            float xNum = float.Parse(x.text);
+            float yNum = float.Parse(y.text);
+            float zNum = float.Parse(z.text);
+            float angleNum = float.Parse(angle.text);
+
+            transform.rotation *= Quaternion.AngleAxis(angleNum, new Vector3(xNum, yNum, zNum));
+        }
+
+
 
     }
 
